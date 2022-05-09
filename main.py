6570218bd274996afba6a523e55541b9d005cc72
makwerik <serezha0790@mail.ru>
2022-05-09 13:23:39 +0300

    Configuring the network card configuration: Changing the IP address, dns, proxy. And setting the default settings

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+import time
+import yaml
+
+
+class Ethernet():
+    """Смена IP адреса, шлюза,
+    DNS сервера,включение proxy и так же возврат к стандартным настройкам"""
+    def __init__(self, cfg):
+        with open('cfg.yaml') as file:
+            params = yaml.load(file,Loader=yaml.FullLoader)
+        self.ip = params['ip']
+        self.dns = params['dns']
+        self.dns2 = params['dns2']
+        self.proxy = params['proxy']
+        self.ip_def = params['ip_def']
+        self.dns_def = params['dns_def']
+        self.proxy_def = params['proxy_def']
+
+    def work_setting(self):
+        """Смена ip,dns,proxy"""
+        subprocess.call(self.ip,shell=True)
+        subprocess.call(self.dns, shell=True)
+        subprocess.call(self.dns2, shell=True)
+        subprocess.call(self.proxy, shell=True)
+
+
+
+    def home_setting(self):
+        """Возврат к default настройкам Windows"""
+        subprocess.call(self.ip_def, shell=True)
+        subprocess.call(self.dns_def, shell=True)
+        subprocess.call(self.proxy_def, shell=True)
+
+
+if __name__ == '__main__':
+    Et = Ethernet(r'cfg.yaml')
+    ask = input('Home or Work? ')
+    if ask == 'Home':
+        Et.home_setting()
+    elif ask == 'Work':
+        Et.work_setting()
+
