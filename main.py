4bde1b7b1d19404631be53f3af4fa181508df628
makwerik <serezha0790@mail.ru>
2022-05-10 22:18:45 +0300

    Added requirements.txt Moved the configuration file to a separate data folder

--- main.py
+++ main.py
@@ -4,39 +4,48 @@ import time
 import yaml
 
 
-class Ethernet():
-    """Смена IP адреса, шлюза,
-    DNS сервера,включение proxy и так же возврат к стандартным настройкам"""
-    def __init__(self, cfg):
-        with open('cfg.yaml') as file:
-            params = yaml.load(file,Loader=yaml.FullLoader)
-        self.ip = params['ip']
-        self.dns = params['dns']
-        self.dns2 = params['dns2']
-        self.proxy = params['proxy']
-        self.ip_def = params['ip_def']
-        self.dns_def = params['dns_def']
-        self.proxy_def = params['proxy_def']
+class Ethernet:
+    """
+    Смена IP-адреса, шлюза,
+    DNS-сервера, включение Proxy, а также возврат к стандартным настройкам
+    """
+    def __init__(self, cfg_filepath=None):
+        if not cfg_filepath:
+            print("Please specify the full path to file with params!")
+        else:
+            with open(cfg_filepath, "r", encoding="utf8") as file:
+                params = yaml.load(file, Loader=yaml.FullLoader)
+
+            self.ip = params['ip']
+            self.dns = params['dns']
+            self.dns2 = params['dns2']
+            self.proxy = params['proxy']
+            self.ip_def = params['ip_def']
+            self.dns_def = params['dns_def']
+            self.proxy_def = params['proxy_def']
 
     def work_setting(self):
-        """Смена ip,dns,proxy"""
-        subprocess.call(self.ip,shell=True)
+        """
+        Смена IP, DNS, Proxy
+        """
+        subprocess.call(self.ip, shell=True)
         subprocess.call(self.dns, shell=True)
         subprocess.call(self.dns2, shell=True)
         subprocess.call(self.proxy, shell=True)
 
-
-
     def home_setting(self):
-        """Возврат к default настройкам Windows"""
+        """
+        Возврат к default-настройкам
+        """
         subprocess.call(self.ip_def, shell=True)
         subprocess.call(self.dns_def, shell=True)
         subprocess.call(self.proxy_def, shell=True)
 
 
 if __name__ == '__main__':
-    Et = Ethernet(r'cfg.yaml')
+    Et = Ethernet("data/cfg.yaml")
     ask = input('Home or Work? ')
+
     if ask == 'Home':
         Et.home_setting()
     elif ask == 'Work':
